98fbf79f0d14eba660009b833d3ed8c7f780ac8c
prafullajoshi970 <97543959+prafullajoshi970@users.noreply.github.com>
2023-04-23 23:46:44 +0530

    firstCommit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+
+
+function App() {
+
+  
+
+
+
+  return (
+    <div className="main">
+   <div className="main1">
+
+    <div className="imgdiv">
+      <button className="btn1">+</button>
+      <img src='https://media.istockphoto.com/id/1351285381/photo/happy-young-african-american-woman-looking-at-camera.jpg?b=1&s=170667a&w=0&k=20&c=N31nPlncl1RGZ2IxfQdXTCl9LFN3pD31mWYhzxeRpdY='alt=""></img>
+    </div>
+
+    <div className="header-div">
+     <h1>Alexandra Caulea</h1>
+     <p>I enjoy a cup of coffee drinking Every day</p>
+    </div>
+
+
+
+<div className="list">
+<div className="left-side">
+  <h3>26</h3>
+  <h3>Shoot</h3>
+</div>
+<div  className="center">
+<h3>94</h3>
+<h3>Followers</h3>
+</div>
+<div  className="right-side">
+<h3>39</h3>
+<h3>Followings</h3>
+</div>
+
+</div>
+
+   </div>
+    </div>
+  );
+}
+
+export default App;
